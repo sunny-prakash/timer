29644fd5848097ae04ff35606922acab2ef39772
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-05-11 15:32:06 +0530

    removed saving in local storage and did some changes

--- timer/src/App.js
+++ timer/src/App.js
@@ -13,48 +13,39 @@ class App extends React.Component {
             input: "",
             countDownDate: 0,
             showInput: "",
-            msg: "Timer ends on",
+            msg: "Enter date to start Timer",
             buttonToggle: true,
         };
     }
 
-    componentDidMount() {
-        let userTime = localStorage.getItem("countDownDate") !== null ? JSON.parse(localStorage.getItem("countDownDate")) : "";
-        let showMsg = localStorage.getItem("showInput") !== null ? JSON.parse(localStorage.getItem("showInput")) : "";
-        let buttonShow = localStorage.getItem("buttonToggle") !== null ? JSON.parse(localStorage.getItem("buttonToggle")) : "";
-        this.setState({ countDownDate: userTime, showInput: showMsg, buttonToggle: buttonShow });
-        if (this.state.countDownDate !== null && this.state.countDownDate !== "") this.countDownStart();
-    }
-
-    componentDidUpdate() {
-        localStorage.setItem("countDownDate", JSON.stringify(this.state.countDownDate));
-        localStorage.setItem("showInput", JSON.stringify(this.state.showInput));
-        localStorage.setItem("buttonToggle", JSON.stringify(this.state.buttonToggle));
-    }
+    
     onChangeInput = (e) => {
         this.setState({ input: e.target.value });
     };
     setTimer = async () => {
+        this.setState({msg:""});
+        // console.log("inside setTimer");
         let date = await new Date(this.state.input).getTime();
-        await this.setState({ buttonToggle: false, showInput: this.state.input, countDownDate: date, input: "" });
-
-        if (isNaN(this.state.countDownDate)) {
-            this.resetTimer();
+        this.setState({ countDownDate: date });
+        // console.log("countDownDate ",this.state.countDownDate);
+        if (this.state.countDownDate === "0" || this.state.input ==="0" ||  isNaN(this.state.countDownDate)||this.state.countDownDate === "" || this.state.countDownDate === null) {
+            console.log("inside isNaN(this.state.countDownDate)");
+            this.setState({msg:"Enter Valid Date",input: "",buttonToggle: false,});
+            
             return;
         }
+        this.setState({buttonToggle: false, showInput: this.state.input, input: "" });
         this.countDownStart();
     };
 
     countDownStart = () => {
-        if (this.state.countDownDate === "" || this.state.countDownDate === null) {
-            return;
-        }
-        this.startInterval = setInterval(async () => {
-            var presentTime = await new Date().getTime();
+        // console.log("going to start startInterval");
+        this.startInterval = setInterval(() => {
+            var presentTime = new Date().getTime();
             var distance = this.state.countDownDate - presentTime;
             if (isNaN(distance)) return;
             distance <= 0
-                ? this.setState({ msg: "Invalid date type or Time has already ended on" })
+                ? this.setState({ msg: "Time has already ended on" })
                 : this.setState({
                       msg: "Timer ends on",
                       days: Math.floor(distance / (1000 * 60 * 60 * 24)),
@@ -67,14 +58,14 @@ class App extends React.Component {
 
     resetTimer = () => {
         clearInterval(this.startInterval);
-        this.setState({ days: "0", hours: "0", minutes: "0", seconds: "0", input: "", countDownDate: "", showInput: "", msg: "Timer ends on", buttonToggle: true });
+        this.setState({ days: "0", hours: "0", minutes: "0", seconds: "0", input: "", countDownDate: "", showInput: "", msg: "Enter date to start Timer", buttonToggle: true });
     };
 
     render() {
         return (
             <div className="tc mt4">
-                <h1 className="dark-blue">{"Countdown Timer"}</h1>
-                <div className="mt5 washed-blue">
+                <h1 className="dark-blue f1">{"Countdown Timer"}</h1>
+                <div className="mt5 washed-blue f3">
                     <h3>{`${this.state.msg} ${this.state.showInput}`}</h3>
                     <h3>{`${this.state.days} Days ${this.state.hours} Hours ${this.state.minutes} Mins ${this.state.seconds} Seconds`}</h3>
 
@@ -87,7 +78,7 @@ class App extends React.Component {
                         </div>
                     ) : (
                         <button className="ml3 bn br3 pa2 fw5 bg-light-red" onClick={() => this.resetTimer()}>
-                            {"Stop Countdown"}
+                            {"RESET"}
                         </button>
                     )}
                 </div>
